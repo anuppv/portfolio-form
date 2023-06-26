4005a0ff73371632acbd666ef0ff8a606b123d41
Anoop <anup.sur@gmail.com>
2023-06-26 13:59:11 +0530

    second commit

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 
 function validateName(){
-    let tname = document.getElementById("name").value;
+    let text = document.getElementById("name").value;
     let regex = /^[a-zA-Z]+$/;
-    if(regex.test(tname)){
-        document.getElementById("warning-name").innerHTML("Success")
+    if(regex.test(text)){
+        document.getElementById("warning-name").innerHTML="Your name is validated"
         document.getElementById("warning-name").style.color="green"
         return true;
     }
     else{
-        document.getElementById("warning-name").innerHTML("Failed")
+        document.getElementById("warning-name").innerHTML="Please enter correct name"
         document.getElementById("warning-name").style.color="red"
         return false;
     }
 }
 
 function validateEmail(){
-    let temail = document.getElementById("email").value;
+    let text = document.getElementById("email").value;
     let regex = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9\.-]{2,20})\.([a-z]{2,6})(.[a-z]{2,6})?$/;
-    if(regex.test(temail)){
-        document.getElementById("warning-email").innerHTML("Success")
+    if(regex.test(text)){
+        document.getElementById("warning-email").innerHTML="Your email is Validated"
         document.getElementById("warning-email").style.color="green"
         return true;
     }
     else{
-        document.getElementById("warning-email").innerHTML("Failed")
+        document.getElementById("warning-email").innerHTML="Emter a valid email address"
         document.getElementById("warning-email").style.color="red"
         return false;
     }
 }
 
 function validatePhone(){
-    let tphone = document.getElementById("phone").value;
+    let text = document.getElementById("phone").value;
     let regex = /^[0-9]{10}$/;
-    if(regex.test(tphone)){
+    if(regex.test(text)){
+        document.getElementById("warning-phone").innerHTML="Your Phone number is validated"
         document.getElementById("warning-phone").style.color="green"
-        document.getElementById("warning-phone").innerHTML("Success")
         return true;
     }
     else{
-        document.getElementById("warning-phone").innerHTML("Failed")
+        document.getElementById("warning-phone").innerHTML="Enter a valid phone number"
         document.getElementById("warning-phone").style.color="red"
         return false;
     }
 }
 
 function validateMessage(){
-    let tmessage = document.getElementById("message").value;
-    let regex = /^[a-zA-Z]+$/;
-    if(regex.test(tmessage)){
-        document.getElementById("warning-message").innerHTML("Success")
+    let text = document.getElementById("message").value;
+    let regex = /^[a-zA-Z0-9\.-=+()*&^%$#@!?><":';||}{[]]+$/;
+    if(regex.test(text)){
+        document.getElementById("warning-message").innerHTML="Your message is successfully validated"
         document.getElementById("warning-message").style.color="green"
         return true;
     }
     else{
+        document.getElementById("warning-message").innerHTML="Check and correct your message before submutting"
         document.getElementById("warning-message").style.color="red"
-        document.getElementById("warning-message").innerHTML("Failed")
         return false; 
     }
 }
